9425eb33f9001f4d428f22f0c6d2973af4818b28
tgvoskuilen <tyler.voskuilen@gmail.com>
2014-03-07 17:07:35 -0500

    Simplified reactingDyMFoam solver

--- dynamicRefineBalancedFvMesh/dynamicRefineBalancedFvMesh.H
+++ dynamicRefineBalancedFvMesh/dynamicRefineBalancedFvMesh.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2013 Tyler Voskuilen
+    \\  /    A nd           | Copyright (C) 2014 Tyler Voskuilen
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
